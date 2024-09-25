19769dce9d25e5b98fb70b50b381851b85d04b1e
Kaoruko Waguri <myalbedosan21@gmail.com>
2024-09-25 16:15:21 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'offline-cache-v1';
+const urlsToCache = [
+    '/',
+    '/main_index.html',
+    '/index.css',
+    '/index.js',
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Caching offline assets');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch from Cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached file or fetch from network
+                return response || fetch(event.request);
+            })
+    );
+});
